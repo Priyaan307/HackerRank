69cd5ab6455123ad851a5ffd8719015a46208af6
priyadarshani <priyarrsdce@gmail.com>
2023-12-12 22:56:52 +0530

    Day55

---
+++ numberLineJump.js
@@ -0,0 +1,15 @@
+function kangaroo(x1, v1, x2, v2) {
+    if (x1 === x2 && v1 === v2) {
+        return 'YES';
+    }
+    if (v1 === v2) {
+        return 'NO';
+    }
+    if ((x2 - x1) % (v1 - v2) === 0 && (x2 - x1) / (v1 - v2) >= 0) {
+        return 'YES';
+    } else {
+        return 'NO';
+    }
+}
+console.log(kangaroo(0, 3, 4, 2)); // Output: 'YES'
+console.log(kangaroo(0, 2, 5, 3)); // Output: 'NO'
