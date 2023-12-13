092301b8c398f74506e4cf767a91314efdd894c8
priyadarshani <priyarrsdce@gmail.com>
2023-12-13 22:52:09 +0530

    Day56

---
+++ miniMaxSum.js
@@ -0,0 +1,28 @@
+// M-1
+// function miniMaxSum(arr) {
+//   let maxSum = 0;
+//   let minSum = 0;
+//   let maxValue = Math.max(...arr);
+//   let minValue = Math.min(...arr);
+//   for (i = 0; i < arr.length; i++) {
+//     if (arr[i] !== minValue) {
+//       maxSum = maxSum + arr[i];
+//     }
+//   }
+//   for (i = 0; i < arr.length; i++) {
+//     if (arr[i] !== maxValue) {
+//       minSum = minSum + arr[i];
+//     }
+//   }
+//   console.log(`${minSum} ${maxSum}`);
+  
+// }
+// // console.log(maxValue);
+// // console.log(arr);
+
+// // console.log(minValue);
+// // console.log(arr);
+
+// miniMaxSum([1, 2, 3, 4, 5]); // Output: 10 14
+
+//M-2
