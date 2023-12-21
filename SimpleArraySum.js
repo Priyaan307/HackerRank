3dde96e406765a77a08e9281700dedcf219d504b
priyadarshani <priyarrsdce@gmail.com>
2023-12-21 23:25:48 +0530

    updated dir

--- SimpleArraySum.js
+++
@@ -1,9 +0,0 @@
-function simpleArraySum(ar) {
-    let sum = 0;
-    for(let i = 0; i<ar.length; i++){
-        sum += ar[i]
-    }
-return sum;
-}
-let ar =[1,2,3]
-console.log(simpleArraySum(ar));
