5791c8fea37793f415a9d40c074509377de2493b
priyadarshani <priyarrsdce@gmail.com>
2023-12-26 19:04:43 +0530

    Drawing Book

--- Day62/DrawingBook.js
+++ Day62/DrawingBook.js
@@ -1,6 +1,33 @@
+// function pageCount(n, p) {
+//     const turnsFromFront = Math.floor(p / 2);
+// const turnsFromBack = Math.floor((n / 2) - (p / 2));
+// return Math.min(turnsFromFront, turnsFromBack);
+// }
+// console.log(pageCount(6, 2));
+
+
 function pageCount(n, p) {
-    const turnsFromFront = Math.floor(p / 2);
-const turnsFromBack = Math.floor((n / 2) - (p / 2));
-return Math.min(turnsFromFront, turnsFromBack);
-}
-console.log(pageCount(6, 2));
+    let turnsFromFront = 0;
+    let turnsFromBack = 0;
+  
+    // Calculate turns from front
+    for (let i = 0; i <= p; i += 2) {
+      if (i === p || i + 1 === p) {
+        break;
+      }
+      turnsFromFront++;
+    }
+  
+    // Calculate turns from back
+    for (let i = n; i >= p; i -= 2) {
+      if (i === p || i - 1 === p) {
+        break;
+      }
+      turnsFromBack++;
+    }
+  
+    return Math.min(turnsFromFront, turnsFromBack);
+  }
+  
+  console.log(pageCount(6, 2));
+  
