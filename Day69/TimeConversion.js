e2737e382c66c42c7b69f26d3af89fe6a72cad4c
priyadarshani <priyarrsdce@gmail.com>
2023-12-26 22:07:23 +0530

    Day69

--- Day69/TimeConversion.js
+++ Day69/TimeConversion.js
@@ -1,22 +1,21 @@
 function timeConversion(s) {
-    // Extract hours, minutes, and seconds
+    
     const timeArray = s.slice(0, 8).split(':');
     const hours = parseInt(timeArray[0], 10);
     const minutes = timeArray[1];
     const seconds = timeArray[2];
 
-    // Check if it's AM or PM
+    
     const period = s.slice(8, 10);
 
-    // Convert to 24-hour format
+    
     let militaryHours = hours;
     if (period === 'PM' && hours < 12) {
         militaryHours += 12;
     } else if (period === 'AM' && hours === 12) {
         militaryHours = '00';
     }
-
-    // Construct the result string
+    
     const militaryTime = `${militaryHours.toString().padStart(2, '0')}:${minutes}:${seconds}`;
     
     return militaryTime;
