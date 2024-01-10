2f95db543353c8f42ee8f0b93efdaf909cff9b23
priyadarshani <priyarrsdce@gmail.com>
2024-01-10 20:47:41 +0530

    Beautiful Triplets

--- Day84/BeautifulTriplets.js
+++ Day84/BeautifulTriplets.js
@@ -1,3 +1,4 @@
+/* 
 function beautifulTriplets(d, arr) {
     let count = 0;
 
@@ -24,3 +25,16 @@ const arr = [1, 2, 4, 5, 7, 8, 10];
 // Output
 const result = beautifulTriplets(d, arr);
 console.log(result); // Output: 3
+*/
+
+function beautifulTriplets(d, arr) {
+    let count = 0;
+
+    for (let i = 0; i < arr.length - 2; i++) {
+        if (arr.includes(arr[i] + d) && arr.includes(arr[i] + 2 * d)) {
+            count++;
+        }
+    }
+
+    return count;
+}
